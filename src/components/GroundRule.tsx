a9d5869e8ff4cf43f3bb4d2209e8f081b7943b61
Soohyun Jung <sasha1107@naver.com>
2023-10-22 23:53:15 +0900

    style: 그라운드룰 스타일

--- src/components/GroundRule.tsx
+++ src/components/GroundRule.tsx
@@ -1,6 +1,15 @@
 import { useState, useEffect } from 'react';
 import styles from './GroundRule.module.scss';
 import SaveButton from './Button';
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  Typography,
+  Grid,
+  Box,
+  Divider,
+} from '@mui/material';
 
 const GROUND_RULE = [
   '반말은 절대로 하지 않고 높임말도 하지 않으려고 합니다.',
@@ -9,7 +18,7 @@ const GROUND_RULE = [
   '알려줄 때에도 굳이 지적하는 식의 표현은 지양하도록 합니다.',
   '적극적으로 모르는 것에 대해 질문합니다.',
   '왜 지금 상황이 이렇게 힘든지 말하기 보다 어떻게 하면 이 도전과제를 해결할 수 있을지에 집중합니다',
-  ''
+  // '',
 ];
 
 const GroundRule = () => {
@@ -27,38 +36,66 @@ const GroundRule = () => {
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
   };
-useEffect(() => {
-  // getGroundrule = () => {
-
-  // }
-},[])
+  useEffect(() => {
+    // getGroundrule = () => {
+    // }
+  }, []);
   return (
     <>
-      <div className={styles.groundrule_container}>
-        <div className={styles.groundrule_title}>
-          <span>그라운드 룰</span>
-        </div>
-        <form action="" className={styles.groundrule_form}>
-          <div className={styles.groundrule_savebutton}>
-            <SaveButton />
-          </div>
-          {GROUND_RULE.map((rule, index) => {
-            return (
-              <div key={index} className={styles.groundrule_box}>
-                <label htmlFor={rule} className={styles.groundrule_label}>
-                  <input
-                    type="checkbox"
-                    id={rule}
-                    onChange={() => handleCheckboxChange(index)}
-                    checked={GR.includes(index)}
-                  />
-                  <span className={styles.groundrule_rule}>{rule}</span>
-                </label>
-              </div>
-            );
-          })}
-        </form>
-      </div>
+      <form action="" className={styles.groundrule_form}>
+        <Grid container spacing={2}>
+          <Grid item xs={12}>
+            <Box
+              sx={{
+                display: 'flex',
+                justifyContent: 'space-between',
+                alignItems: 'center',
+              }}
+            >
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: 2,
+                }}
+              >
+                <img
+                  src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Symbols/Hamsa.png"
+                  alt="Hamsa"
+                  width="60"
+                  height="60"
+                />
+                <Typography variant="h2">그라운드룰</Typography>
+              </Box>
+              <SaveButton />
+            </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="subtitle1" gutterBottom>
+              팀원과 소통을 원활히 하고, 코드 변경사항을 편리하게 확인하기 위해
+              좋은 커밋메시지를 작성해야 합니다.
+            </Typography>
+            <Divider variant="fullWidth" />
+          </Grid>
+          <Grid item xs={12}>
+            {GROUND_RULE.map((rule, index) => {
+              return (
+                <div key={index} className={styles.groundrule_box}>
+                  <label htmlFor={rule} className={styles.groundrule_label}>
+                    <input
+                      type="checkbox"
+                      id={rule}
+                      onChange={() => handleCheckboxChange(index)}
+                      checked={GR.includes(index)}
+                    />
+                    <span className={styles.groundrule_rule}>{rule}</span>
+                  </label>
+                </div>
+              );
+            })}
+          </Grid>
+        </Grid>
+      </form>
     </>
   );
 };
