19de4aaaab6fd47c63d5847f64015c736b952a9b
a-honey <jungahyeon0512@khu.ac.kr>
2023-10-22 16:53:16 +0900

    Feat: 인트로페이지 api 연동 및 localStorage 데이터 저장

--- src/pages/IntroPage.tsx
+++ src/pages/IntroPage.tsx
@@ -1,11 +1,41 @@
 import TextField from '@mui/material/TextField';
-import { Button, FormControl, Typography } from '@mui/material';
+import { Button, FormControl, Typography, Box } from '@mui/material';
+import { useState } from 'react';
+
 const IntroPage = () => {
-  const handleSubmit = () => {
-    console.log('프로젝트명 변경');
+  const [projectName, setProjectName] = useState('');
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+
+    const requestData = JSON.stringify(projectName);
+    try {
+      const response = await fetch('https://api.pcmk.dppr.me/api/v1/projects', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: requestData,
+      });
+
+      if (response.ok) {
+        const jsonResponse = await response.json();
+        localStorage.setItem('data', JSON.stringify(jsonResponse));
+      } else {
+        console.error('Error:', response.status);
+      }
+    } catch (error) {
+      console.error('Error:', error);
+    }
   };
   return (
-    <main>
+    <Box
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <Typography
         variant="h3"
         gutterBottom
@@ -16,10 +46,19 @@ const IntroPage = () => {
         프로젝트 컨벤션 메이커
       </Typography>
       <FormControl component="form" onSubmit={handleSubmit}>
-        <TextField id="outlined-basic" label="프로젝트명" variant="outlined" />
-        <Button variant="contained">작성</Button>
+        <TextField
+          id="outlined-basic"
+          label="프로젝트명"
+          variant="outlined"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setProjectName(e.target.value)
+          }
+        />
+        <Button type="submit" variant="contained">
+          작성
+        </Button>
       </FormControl>
-    </main>
+    </Box>
   );
 };
 
